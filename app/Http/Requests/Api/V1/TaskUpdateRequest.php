521ab9d7bdcc66b4f1dba8ad92926bc7a5c8811f
Sahan Pasindu Nirmal <sahanpasindu07@gmail.com>
2025-11-05 11:50:54 +0530

    Add Task request validation classes

---
+++ app/Http/Requests/Api/V1/TaskUpdateRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TaskUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'priority' => 'in:low,medium,high',
+            'status' => 'in:todo,in_progress,done',
+            'due_date' => 'nullable|date'
+        ];
+    }
+}
